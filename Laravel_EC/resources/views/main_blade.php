efff3743428d7892bc182f3ec90322c4b8847566
K M <>
2024-09-27 16:26:47 +0900

    no message

---
+++ Laravel_EC/resources/views/main_blade.php
@@ -0,0 +1,234 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>スキルシェアECサイト</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <div class="wrapper">
+        <div class="main-visual">
+            <div class="image-container">
+                <img src="images/kari.jpg" alt="">
+            </div>
+            <h1>Skill Masterとは？</h1>
+        </div>
+        <div class="teach-ranking">
+            <div class="teach-ranking-text">
+                <h1>Ranking</h1>
+                <p>ランキング</p>
+            </div>
+            <div class="slider">
+                <div class="slider-wrapper">
+                    <div class="ranking-list">
+                        <div class="slide">
+                            <div class="item">
+                                <div class="item-content">
+                                    <img src="images/kari.jpg" alt="">
+                                    <p class="item-description">現役エンジニアが教える！<br>即戦力プログラミング講座</p>
+                                    <p class="item-price">¥3,740</p>
+                                    <a href="#" class="item-link">詳細を見る</a>
+                                </div>
+                            </div>
+                            <div class="item">
+                                <div class="item-content">
+                                    <img src="images/kari.jpg" alt="">
+                                    <p class="item-description">現役エンジニアが教える!<br>ハイレベルプログラミング</p>
+                                    <p class="item-price">¥8,640</p>
+                                    <a href="#" class="item-link">詳細を見る</a>
+                                </div>
+                            </div>
+                            <div class="item">
+                                <div class="item-content">
+                                    <img src="images/kari.jpg" alt="">
+                                    <p class="item-description">現役エンジニアが教える!<br>デザイン基礎講！</p>
+                                    <p class="item-price">¥4,230</p>
+                                    <a href="#" class="item-link">詳細を見る</a>
+                                </div>
+                            </div>
+                            <div class="item">
+                                <div class="item-content">
+                                    <img src="images/kari.jpg" alt="">
+                                    <p class="item-description">本格中華料理の極意<br>家庭で味わえる本格中華</p>
+                                    <p class="item-price">¥3,740</p>
+                                    <a href="#" class="item-link">詳細を見る</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="slide">
+                            <div class="item">
+                                <div class="item-content">
+                                    <img src="images/kari.jpg" alt="">
+                                    <p class="item-description">最短で稼げるデザイン習得</p>
+                                    <p class="item-price">¥3,740</p>
+                                    <a href="#" class="item-link">詳細を見る</a>
+                                </div>
+                            </div>
+                            <div class="item">
+                                <div class="item-content">
+                                    <img src="images/kari.jpg" alt="">
+                                    <p class="item-description">フォートナイト建築マスター 入門</p>
+                                    <p class="item-price">¥8,640</p>
+                                    <a href="#" class="item-link">詳細を見る</a>
+                                </div>
+                            </div>
+                            <div class="item">
+                                <div class="item-content">
+                                    <img src="images/kari.jpg" alt="">
+                                    <p class="item-description">見て、聞いて、話して<br>学べる韓国語講座</p>
+                                    <p class="item-price">¥4,230</p>
+                                    <a href="#" class="item-link">詳細を見る</a>
+                                </div>
+                            </div>
+                            <div class="item">
+                                <div class="item-content">
+                                    <img src="images/kari.jpg" alt="">
+                                    <p class="item-description">Let's enjoy Chainease!</p>
+                                    <p class="item-price">¥3,740</p>
+                                    <a href="#" class="item-link">詳細を見る</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+                <div class="button">
+                    <button class="prev">←</button>
+                    <div class="page">
+                        <p>01</p>
+                        <div class="line"></div>
+                        <p>02</p>
+                    </div>
+                    <button class="next">→</button>
+                </div>
+            </div>
+
+            <div class="recomend-wrapper">
+                <div class="recomend-text">
+                    <h1>Recomended</h1>
+                    <p>おすすめ</p>
+                </div>
+                <div class="recomend">
+                    <div class="recomend-list">
+                        <img src="images/kari.jpg" alt="">
+                        <p>初心者向けPythonコース<br>Web制作コース</p>
+                        <p class="item-price">¥5,740</p>
+                        <p class="item-link">詳細を見る</p>
+                    </div>
+                    <div class="recomend-list">
+                        <img src="images/kari.jpg" alt="">
+                        <p>現役デザイナーが教える!<br>デザイン入門講座</p>
+                        <p class="item-price">¥6,270</p>
+                        <p class="item-link">詳細を見る</p>
+                    </div>
+                    <div class="recomend-list">
+                        <img src="images/kari.jpg" alt="">
+                        <p>[初心者脱出]<br>Valorant完全攻略講座</p>
+                        <p class="item-price">¥8,640</p>
+                        <p class="item-link">詳細を見る</p>
+                    </div>
+                    <div class="recomend-list">
+                        <img src="images/kari.jpg" alt="">
+                        <p>現役デザイナーが教える!<br>Webデザイン講座</p>
+                        <p class="item-price">¥6,230</p>
+                        <p class="item-link">詳細を見る</p>
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="field-area">
+                <h1>You can learn</h1>
+                <p>分野</p>
+                <div class="field-content1">
+                    <div class="field-img">
+                        <img src="images/Vector 8.jpg" alt="背景画像">
+                    </div>
+                    <div class="field-text">
+                        <h1 class="h1-text">プログラミング</h1>
+                        <h2>学べる内容</h2>
+                        <ul>
+                            <li>Java</li>
+                            <li>Java</li>
+                            <li>Java</li>
+                            <li>Java</li>
+                        </ul>
+                        <p>あああああああああああああああああああああああ</p>
+                        <h3>saaaaaaaaaaaaaaaaaaa</h3>
+                        <p>詳細を見る</p>
+                    </div>
+                </div>
+
+            </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            <!-- <div class="scene-area">
+                <h1>Scene</h1>
+                <p>シーンや気持ちで探す</p>
+                <div class="scene-list">
+                    <ul>
+                        <li>#音楽したい🎹</li>
+                        <li>#プログラミングを学びたい💻</li>
+                        <li>#資格試験✏️</li>
+                        <li>#料理したい気分🍳</li>
+                        <li>#運動したい気分🏃‍♀️</li>
+                        <li>#デザインしたい🎨</li>
+                        <li>#外国語を学びたい🇺🇸</li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="category-area">
+                <h1>Category</h1>
+                <p>カテゴリーから探す</p>
+                <div class="category-content">
+                    <img src="images/kari.jpg" alt="">
+                    <p>#音楽🎵</p>
+
+                    <img src="images/kari.jpg" alt="">
+                    <p>#プログラミング💻</p>
+
+                    <img src="images/kari.jpg" alt="">
+                    <p>#資格✏️</p>
+
+                    <img src="images/kari.jpg" alt="">
+                    <p>#料理🍳</p>
+
+                    <img src="images/kari.jpg" alt="">
+                    <p>#運動🥊</p>
+
+                    <img src="images/kari.jpg" alt="">
+                    <p>#デザイン🧑‍🎨</p>
+
+                    <img src="images/kari.jpg" alt="">
+                    <p>#E-sports🎮</p>
+
+                    <img src="images/kari.jpg" alt="">
+                    <p>#人気上昇🔥</p>
+
+                    <img src="images/kari.jpg" alt="">
+                    <p>#殿堂入り👑</p>
+                </div>
+            </div>
+
+        </div> -->
+            <script src="js/main.js"></script>
+</body>
+
+</html>
