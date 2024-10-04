e01f4808e3da295c83e0e0a01a84124ed442bb5c
kurihayas <hayamaru0528@gmail.com>
2024-10-04 15:25:13 +0900

    変えたよ

--- Laravel_EC/routes/web.php
+++ Laravel_EC/routes/web.php
@@ -2,6 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
+// このファイルはLaravelのルーティング設定を行います。
+
+// ルート'/'（ホームページ）にアクセスした際の処理を定義しています。
+// 'welcome'ビューを返すように設定されています。
 Route::get('/', function () {
     return view('welcome');
 });
+
+// 上記のコードは、ユーザーがウェブサイトのルートURLにアクセスした時に、
+// 'welcome'という名前のビューファイルを表示するよう指示しています。
+// このビューファイルは通常、resources/views/welcome.blade.phpに配置されています。
